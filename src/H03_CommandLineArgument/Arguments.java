17330ea75d2ff480a6cad68fa8ce462f817beb8a
kenanhs24 <170270713+kenanhs24@users.noreply.github.com>
2024-11-29 22:42:55 -0500

    arguments

--- src/H03_CommandLineArgument/Arguments.java
+++ src/H03_CommandLineArgument/Arguments.java
@@ -1,14 +1,19 @@
 package H03_CommandLineArgument;
 
-public class Arguments {
-    public static void main(String[] args) {
+public class Arguments
+{
+    public static void main(String[] args)
+    {
         // Check if arguments are provided
-        if (args.length == 0) {
+        if (args.length == 0)
+        {
             System.out.println("No arguments provided.");
-        } else {
+        } else
+        {
             System.out.println("Command-line arguments:");
             // Iterate over and print each argument
-            for (String argument : args) {
+            for (String argument : args)
+            {
                 System.out.println(argument);
             }
         }
