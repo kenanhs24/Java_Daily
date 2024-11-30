bdc1a9c6822c288d9db359c33ba98747fed7775a
kenanhs24 <170270713+kenanhs24@users.noreply.github.com>
2024-11-29 21:05:59 -0500

    Print Integer in Java

---
+++ src/H02_PrintInterger/Integers.java
@@ -0,0 +1,21 @@
+package H02_PrintInterger;
+
+public class Integers
+{
+    public static void main(String[] args)
+    {
+        int H;  // declaring a variable
+
+        // using for loop to repeat instructions executions
+        System.out.println("Prints the current value of H in each iteration");
+        for ( H = 1; H<= 7 ; H++){  // my fav number 7
+
+            System.out.println(H);  // Output will be 1,2,3,4,5,6,7
+        }
+
+        System.out.println("After the loop ends, print the value of H");
+
+         System.out.println(H);  // Output will be 8
+    }
+
+}
